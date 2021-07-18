5d152bb3f7085028eafb08ae4b7af44225db66d7
KNCupat101 <kenneththegreat101@gmail.com>
2021-07-18 11:27:02 +0800

    Enabled Scroll Bar

--- Raceup Autocare/Raceup Autocare/ShowListofOrderPartsForm.Designer.cs
+++ Raceup Autocare/Raceup Autocare/ShowListofOrderPartsForm.Designer.cs
@@ -662,6 +662,7 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoScroll = true;
             this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(17)))), ((int)(((byte)(17)))), ((int)(((byte)(17)))));
             this.ClientSize = new System.Drawing.Size(1459, 870);
             this.Controls.Add(this.panelDesktop);
